78eef48ef1daea77b59242f6aca59326128b25ae
yogendrasywork <yogendrasywork@gmail.com>
2026-02-12 15:58:41 +0530

    splash screen animation done

--- android/app/src/main/kotlin/com/fertilityshare/app/fertilityshare/MainActivity.kt
+++ android/app/src/main/kotlin/com/fertilityshare/app/fertilityshare/MainActivity.kt
@@ -3,13 +3,20 @@ package com.fertilityshare.app.fertilityshare
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity: FlutterActivity() {
+    private var keepSplashScreenOn = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Handle the splash screen transition.
-        installSplashScreen()
-
-
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        // Keep the splash screen visible for 4 seconds
+        splashScreen.setKeepOnScreenCondition { keepSplashScreenOn }
+        Handler(Looper.getMainLooper()).postDelayed({
+            keepSplashScreenOn = false
+        }, 4000)
     }
 }
